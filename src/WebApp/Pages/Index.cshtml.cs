e48d7fc1d0f742adc5f360891574a2963497aa65
Tural Hasanov <hasanovtural11@gmail.com>
2025-07-11 16:43:34 +0200

    Add updated text and simplify IndexModel
    This commit introduces a new paragraph in the Index view and refactors the IndexModel by removing unnecessary logging dependencies.
    
    - ✨ Added a new paragraph with "Updated text" in `Index.cshtml`
    - 🔧 Removed `ILogger<IndexModel>` and related code in `Index.cshtml.cs`
    
    - Tural Hasanov

--- src/WebApp/Pages/Index.cshtml.cs
+++ src/WebApp/Pages/Index.cshtml.cs
@@ -1,15 +1,11 @@
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace WebApp.Pages
 {
     public class IndexModel : PageModel
     {
-        private readonly ILogger<IndexModel> _logger;
-
-        public IndexModel(ILogger<IndexModel> logger)
+        public IndexModel()
         {
-            _logger = logger;
         }
 
         public void OnGet()
